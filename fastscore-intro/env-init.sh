819178aa7d7a0b0aaa3c0683d623fa4c264a7129
rehganavon <rehgan.avon@opendatagroup.com>
2018-05-22 12:01:39 -0400

    Update env-init.sh

--- fastscore-intro/env-init.sh
+++ fastscore-intro/env-init.sh
@@ -1,4 +1,4 @@
 ssh root@host01 "echo [[HOST_IP]] host01 >> /etc/hosts"  
 ssh root@host01 "git clone https://github.com/rehganavon/katacoda"  
 ssh root@host01 "docker volume create --name=db"  
-ssh root@host01 "while [ "$STR" != "Done" ]; do sleep 1; done && cd katacoda/fastscore-intro/ && chmod ugo+x ./run.sh && chmod ugo+x ./kafkaesq && ./run.sh" 
+ssh root@host01 "cd katacoda/fastscore-intro/ && chmod ugo+x ./run.sh && chmod ugo+x ./kafkaesq && ./run.sh" 
