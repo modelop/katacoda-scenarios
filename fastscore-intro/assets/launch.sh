3215194b2245af5e1aacc5b3321237a952e8d7cc
rehganavon <rehgan.avon@opendatagroup.com>
2018-10-26 10:17:50 -0400

    Update launch.sh

--- fastscore-intro/assets/launch.sh
+++ fastscore-intro/assets/launch.sh
@@ -1,7 +1,8 @@
 echo "Starting FastScore..."
-while [ `fastscore schema show gbm_output | wc -l` > 6 ]
+while [ `fastscore schema show gbm_output | wc -l` -lt 3 ]
 do
   sleep 1
 done
 
-echo "FastScore Started"
+echo "FastScore Started."
+fastscore connect https://localhost:443
