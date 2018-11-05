b875f3d3b90fc434d1ae553988e3f64cca28d4a5
Ben Hall <Ben@Benhall.me.uk>
2018-11-05 22:49:25 +0000

    Update launch.sh

--- fastscore-intro/assets/launch.sh
+++ fastscore-intro/assets/launch.sh
@@ -1,8 +1,8 @@
 echo "Starting FastScore..."
+fastscore connect https://localhost:443
 while [ `fastscore schema show gbm_output | wc -l` -lt 3 ]
 do
   sleep 1
 done
 
 echo "FastScore Started."
-fastscore connect https://localhost:443
